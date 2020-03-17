a0ab4b0c769408bb7d52823bb60608819ce5ba9a
dilian bilchev <dilian.bilchev@tick42.com>
2020-03-17 15:26:00 +0200

    G4E-2018 Update notification demo with latest API

--- notifications/Notifications/App.xaml.cs
+++ notifications/Notifications/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using Tick42;
+using Tick42.StartingContext;
 
 namespace WPFApp
 {
@@ -15,14 +16,50 @@ namespace WPFApp
     /// </summary>
     public partial class App : Application
     {
-        public static Glue42 Glue;
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            Glue = new Glue42();
-            Glue.Initialize("MyDemo");
+            InitializeGlue();
+        }
+
+        private void InitializeGlue()
+        {
+            var initializeOptions = new InitializeOptions()
+            {
+                ApplicationName = "My Glue Notification Demo",
+                InitializeTimeout = TimeSpan.FromSeconds(5)
+            };
+
+            Glue42.InitializeGlue(initializeOptions)
+                .ContinueWith((glue) =>
+                {
+                    //unable to register glue
+                    if (glue.Status == TaskStatus.Faulted)
+                    {
+                        ShowMainWindow();
+                        return;
+                    }
+
+                    //register glue
+                    var glueInstance = glue.Result;
+                    GetMainWindow()?.RegisterGlue(glueInstance);
+
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private MainWindow GetMainWindow()
+        {
+            return Windows.OfType<MainWindow>().FirstOrDefault();
+        }
+
+        private void ShowMainWindow()
+        {
+            var mainWindow = GetMainWindow();
+            if (mainWindow != null)
+            {
+                mainWindow.Visibility = Visibility.Visible;
+            }
         }
     }
 }
--- notifications/Notifications/MainWindow.xaml.cs
+++ notifications/Notifications/MainWindow.xaml.cs
@@ -27,24 +27,80 @@ namespace WPFApp
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window, INotificationHandler
-    {        
+    {
+        #region Dependency Properties
+
+        public static readonly DependencyProperty ConnectionStatusDescriptionProperty = DependencyProperty.Register("ConnectionStatusDescription", typeof(string), typeof(MainWindow));
+        public static readonly DependencyProperty ConnectionStatusColorProperty = DependencyProperty.Register("ConnectionStatusColor", typeof(Brush), typeof(MainWindow));
+
+        public string ConnectionStatusDescription
+        {
+            get
+            {
+                return GetValue(ConnectionStatusDescriptionProperty).ToString();
+            }
+            set
+            {
+                SetValue(ConnectionStatusDescriptionProperty, value);
+            }
+        }
+
+        public Brush ConnectionStatusColor
+        {
+            get
+            {
+                return (Brush)GetValue(ConnectionStatusColorProperty);
+            }
+            set
+            {
+                SetValue(ConnectionStatusColorProperty, value);
+            }
+        }
+
+        #endregion
+
+        private Glue42 _glue;
         public MainWindow()
         {
             InitializeComponent();
 
-            var gwOptions = App.Glue.GlueWindows?.GetStartupOptions() ?? new GlueWindowOptions();
-            gwOptions.WithType(GlueWindowType.Tab);
-            gwOptions.WithTitle("Notification Publisher");
-
-            // register the window 
-            App.Glue.GlueWindows?.RegisterWindow(this, gwOptions);
-
-            App.Glue.Interop.RegisterService<INotificationHandler>(this);
+            DataContext = this;
+            Visibility = Visibility.Hidden;
+            UpdateUI(false);
         }
 
-      
+        internal void RegisterGlue(Glue42 glue)
+        {
+            _glue = glue;
+            UpdateUI(true);
+
+            //bounds are optional. With them we will just set initial placement of the application
+            var defaultBounds = new GlueWindowBounds()
+            {
+                X = (int)((SystemParameters.PrimaryScreenWidth / 2) - (Width / 2)),
+                Y = (int)((SystemParameters.PrimaryScreenHeight / 2) - (Height / 2)),
+                Width = (int)Width,
+                Height = (int)Height
+            };
+            var gwOptions = glue.GetStartupWindowOptions("Notification Publisher", defaultBounds);
+            gwOptions.WithType(GlueWindowType.Tab);
+
+            //register the window
+            glue.GlueWindows?.RegisterWindow(this, gwOptions);
+
+            //register notification service
+            glue.Interop.RegisterService<INotificationHandler>(this);
+        }
+
         private void OnSendNotificationClick(object sender, RoutedEventArgs e)
         {
+            if(_glue == null)
+            {
+                //glue is not initialized
+                return;
+            }
+
+            //object state
             var parameters = new List<GlueMethodParameter>()
             {
                 new GlueMethodParameter("customerId", new GnsValue("11"))
@@ -57,7 +113,7 @@ namespace WPFApp
             };
 
             var notification = new DesktopNotification(Title.Text,
-                NotificationSeverity.Low,
+                (NotificationSeverity)Enum.Parse(typeof(NotificationSeverity), Severity.Text),
                 "type",
                 Description.Text,
                 "category",
@@ -66,12 +122,17 @@ namespace WPFApp
                 actions
             );
 
-            App.Glue.Notifications.Publish(notification);
+            _glue.Notifications.Publish(notification);
         }
 
         public void AcceptNotification(string customerId)
         {
+            //accept handler with notification object state
+        }
 
+        public void RejectNotification(string customerId)
+        {
+            //reject handler with notification object state
         }
 
         public void Dispose()
@@ -79,9 +140,18 @@ namespace WPFApp
 
         }
 
-        public void RejectNotification(string customerId)
+        private void UpdateUI(bool isConnected)
         {
+            var statusMessage = isConnected ? "Connected" : "Disconnected";
+            var statusColor = isConnected ? Colors.LightGreen : Colors.LightPink;
 
+            ConnectionStatusDescription = statusMessage;
+            ConnectionStatusColor = new SolidColorBrush(statusColor);
+
+            Title.IsEnabled = isConnected;
+            Description.IsEnabled = isConnected;
+            Severity.IsEnabled = isConnected;
+            ButtonNotify.IsEnabled = isConnected;
         }
     }
 }
