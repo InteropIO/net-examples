80d5e119625a1def2aae70cf5ad93473e3013227
dilian bilchev <dilian.bilchev@tick42.com>
2020-03-17 13:40:03 +0200

    G4E-2018 Update shared context demo Create common solution for publisher and subscriber projects

---
+++ shared-contexts/SharedContextPub/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tick42;
+using Tick42.Contexts;
+using Tick42.StartingContext;
+
+namespace SharedContextPub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var initializeOptions = new InitializeOptions()
+            {
+                ApplicationName = "Shared Context Publisher",
+                IncludedFeatures = GDFeatures.UseContexts,
+                InitializeTimeout = TimeSpan.FromSeconds(5)
+            };
+
+            Glue42.InitializeGlue(initializeOptions)
+                .ContinueWith((glue) =>
+                {
+                    //unable to register glue
+                    if (glue.Status == TaskStatus.Faulted)
+                    {
+                        Console.WriteLine("Unable to initialize Glue42!");
+                        return;
+                    }
+
+                    var glueInstance = glue.Result;
+                    StartUpdatingContext(glueInstance);
+
+                    Console.WriteLine("Context publisher is started...");
+                });
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async void StartUpdatingContext(Glue42 glue)
+        {
+            int data = 0;
+
+            // update TestContext every second incrementing the value of data
+            var context = await glue.Contexts.GetContext("TestContext");
+            ThreadPool.QueueUserWorkItem((c) =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    context["data"] = data++;
+                }
+            });
+        }
+    }
+}
---
+++ shared-contexts/SharedContextSub/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tick42;
+using Tick42.Contexts;
+using Tick42.StartingContext;
+
+namespace SharedContextSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var initializeOptions = new InitializeOptions()
+            {
+                ApplicationName = "Shared Context Subscriber",
+                IncludedFeatures = GDFeatures.UseContexts,
+                InitializeTimeout = TimeSpan.FromSeconds(5)
+            };
+
+            Glue42.InitializeGlue(initializeOptions)
+                .ContinueWith((glue) =>
+                {
+                    //unable to register glue
+                    if (glue.Status == TaskStatus.Faulted)
+                    {
+                        Console.WriteLine("Unable to initialize Glue42!");
+                        return;
+                    }
+
+                    var glueInstance = glue.Result;
+                    ListenForContextUpdates(glueInstance);
+
+                    Console.WriteLine("Context subscriber is started...");
+                });
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async void ListenForContextUpdates(Glue42 glue)
+        {
+            // Listen for updates in TestContext
+            var context = await glue.Contexts.GetContext("TestContext");
+            context.ContextUpdated += OnContextUpdated;
+        }
+
+        private static void OnContextUpdated(object sender, Tick42.Contexts.ContextUpdatedEventArgs e)
+        {
+            Console.WriteLine("Context updated: " + new
+            {
+                (sender as IContext).ContextName,
+                Added = "[" + string.Join(", ", e.Added.Select(x => x + "")) + "]",
+                Updated = "[" + string.Join(", ", e.Updated.Select(x => x + "")) + "]",
+                Removed = "[" + string.Join(", ", e.Removed.Select(x => x + "")) + "]",
+            });
+        }
+    }
+}
