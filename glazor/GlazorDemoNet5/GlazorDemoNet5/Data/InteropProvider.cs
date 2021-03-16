bbbba0a91da3875dbc6a9725f15d56733f808a17
tomastomovtick <tomas.tomov@tick42.com>
2021-03-16 16:21:30 +0200

    referenced latest glue interop library

--- glazor/GlazorDemoNet5/GlazorDemoNet5/Data/InteropProvider.cs
+++ glazor/GlazorDemoNet5/GlazorDemoNet5/Data/InteropProvider.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GlazorDemoNet5.Data
@@ -15,39 +16,39 @@ namespace GlazorDemoNet5.Data
     public class InteropProvider
     {
         private readonly IJSRuntime jsRuntime_;
+        private static TaskCompletionSource<IGlueInterop> interopTcs_;
+        private static TaskCompletionSource<IGlueChannels> channelsTcs_;
+
 
         public InteropProvider(IJSRuntime jsRuntime)
         {
             jsRuntime_ = jsRuntime;
         }
 
-        public static IGlueInterop Interop { get; private set; }
 
+        public static IGlueInterop Interop { get; private set; }
         public static IGlueChannels Channels { get; private set; }
 
         public async Task<IGlueChannels> InitChannels()
         {
-            if (Channels != null)
+            if (Interlocked.CompareExchange(ref channelsTcs_, new TaskCompletionSource<IGlueChannels>(TaskCreationOptions.RunContinuationsAsynchronously), null) is { } tcs)
             {
-                return Channels;
+                return await tcs.Task.ConfigureAwait(false);
             }
+            var channels = new GlueChannels(await InitInterop().ConfigureAwait(false));
 
-            if (Interop == null)
-            {
-                await InitInterop().ConfigureAwait(false);
-            }
+            channelsTcs_.TrySetResult(channels);
 
-            return Channels = new GlueChannels(Interop);
+            return channels;
         }
 
         public async Task<IGlueInterop> InitInterop()
         {
-            if (Interop != null)
+            if (Interlocked.CompareExchange(ref interopTcs_, new TaskCompletionSource<IGlueInterop>(TaskCreationOptions.RunContinuationsAsynchronously), null) is { } tcs)
             {
-                return Interop;
+                return await tcs.Task.ConfigureAwait(false);
             }
 
-            IAuthenticationProvider authenticationProvider = null;
             GDHostInfo gdInfo = null;
             string gwToken = null;
 
@@ -61,21 +62,11 @@ namespace GlazorDemoNet5.Data
 
             }
 
-            gdInfo = gdInfo ?? new GDHostInfo { GwUrl = "ws://127.0.0.1:8385/", ApplicationName = "Glazor .NET 5"};
+            //In case of external hosting envrionment (browser), the user should match the username used for GD
+            gdInfo = gdInfo ?? new GDHostInfo { ApplicationName = "Glazor .NET 5", User = Environment.UserName };
 
-            var username = Environment.UserName;
-
-            authenticationProvider = gwToken == null ? new LambdaAuthenticationProvider(_ => new AuthenticationDetails(new Dictionary<string, object>()
-            {
-                {"login", username},
-                {"secret", username},
-                {"method", "secret"}
-            }, null)) : new GatewayTokenAuthenticationProvider(gwToken);
-
-            var interop = await GlueInterop.InitInterop(gdInfo.GwUrl, gdInfo.ApplicationName, gdInfo.WindowId, authenticationProvider, new GwProtocolSerializer()).ConfigureAwait(false);
-
-            Interop = interop;
-
+            var interop = await GlueInterop.InitInterop(gdInfo, gwToken).ConfigureAwait(false);
+            interopTcs_.TrySetResult(interop);
             return interop;
         }
 
