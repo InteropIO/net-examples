327cf335a38a32bf0bf134e4cc02d44e0c5068fe
tomastomovtick <tomas.tomov@tick42.com>
2021-03-30 13:35:19 +0300

    Added gd.js

---
+++ glazor/GlazorWebAssembly/GlazorWebAssembly/wwwroot/js/gd.js
@@ -0,0 +1,6 @@
+﻿//We exposed this function in order to easily access the glue42gd property
+function ResolveValue(path, obj) {
+    return path.split('.').reduce(function (prev, curr) {
+        return prev ? prev[curr] : null
+    }, obj || self)
+}
