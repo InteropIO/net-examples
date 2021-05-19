b22ff51ca5f683272f59b0dfd6b1ecaa34e2ae27
tomastomovtick <tomas.tomov@tick42.com>
2021-05-19 18:42:40 +0300

    Added WinForms project to demonstrate Save/Restore state.

---
+++ WindowsFormsDemo/WindowsFormsDemo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WindowsFormsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StateBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StateBox
+            // 
+            this.StateBox.Location = new System.Drawing.Point(170, 187);
+            this.StateBox.Name = "StateBox";
+            this.StateBox.Size = new System.Drawing.Size(365, 26);
+            this.StateBox.TabIndex = 0;
+            this.StateBox.Text = "This text will be saved and restored";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StateBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox StateBox;
+    }
+}
+
---
+++ WindowsFormsDemo/WindowsFormsDemo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tick42;
+using Tick42.StartingContext;
+using Tick42.Windows;
+
+namespace WindowsFormsDemo
+{
+    public partial class Form1 : Form
+    {
+        private Glue42 glue_;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Debugger.Launch();
+
+            var initOptions = new InitializeOptions() { ApplicationName = "MyWinFormsApp" };
+            //the lambda will be called when save layout is called
+            initOptions.SetSaveRestoreStateEndpoint(v => Task.FromResult(StateBox.Text));
+
+            Glue42.InitializeGlue(initOptions)
+                .ContinueWith(async glue =>
+                {
+                    glue_ = glue.Result;
+
+                    //this will be called when the layout is restored
+                    var restoredState = glue_.GetRestoreState<string>();
+
+                    if (restoredState != null)
+                    {
+                        this.Invoke((Action)(() => StateBox.Text = restoredState));
+                    }
+
+                    var window = await glue_.GlueWindows.RegisterWindow(this.Handle, new GlueWindowOptions() { Title = "MyWinformsApp" });
+                });
+        }
+    }
+}
