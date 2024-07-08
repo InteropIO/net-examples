4d9f10ac25278a4a864545088c016f2ec3db2af5
Stanislav Peichev <stanislav.peichev@interop.io>
2024-07-08 16:54:11 +0300

    io-connect-anything - initial commit - launch/save/restore of notepad and paint

---
+++ io-connect-anything/LambdaApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using DOT.Core.Extensions;
+using Tick42;
+using Tick42.AppManager;
+using Tick42.Channels;
+using Tick42.Contexts;
+using Tick42.StartingContext;
+using Tick42.Windows;
+
+namespace io_connect_anything
+{
+    public class LambdaApp<TState> : IGlueAppHandle<TState, object>, IGlueChannelEventHandler
+    {
+        private readonly IntPtr handle_;
+        public Action<IGlueChannelContext, IGlueChannel, IGlueChannel> ChannelChanged;
+        public Action<IGlueChannelContext, IGlueChannel, ContextUpdatedEventArgs> ChannelUpdate;
+        public Func<Task<TState>> GetState;
+        private IGlueWindow glueWindow_;
+        public Action<TState> Init;
+        public Action Shutdown;
+
+        public LambdaApp(IntPtr handle)
+        {
+            handle_ = handle;
+        }
+
+        public IntPtr GetHandle() => handle_;
+
+        void IGlueApp.Shutdown()
+        {
+            Shutdown?.Invoke();
+        }
+
+        void IGlueApp<TState, object>.Initialize(object context, TState state, Glue42 glue,
+            GDStartingContext startingContext,
+            IGlueWindow glueWindow)
+        {
+            glueWindow_ = glueWindow;
+            glueWindow_.ChannelContext?.Subscribe(this);
+            Init?.Invoke(state);
+        }
+
+        Task<TState> IGlueApp<TState, object>.GetState()
+        {
+            return GetState?.Invoke() ?? default(TState).AsCompletedTask();
+        }
+
+        void IGlueChannelEventHandler.HandleChannelChanged(IGlueChannelContext channelContext, IGlueChannel newChannel,
+            IGlueChannel prevChannel)
+        {
+            ChannelChanged?.Invoke(channelContext, newChannel, prevChannel);
+        }
+
+        void IGlueChannelEventHandler.HandleUpdate(IGlueChannelContext channelContext, IGlueChannel channel,
+            ContextUpdatedEventArgs updateArgs)
+        {
+            ChannelUpdate?.Invoke(channelContext, channel, updateArgs);
+        }
+    }
+}
---
+++ io-connect-anything/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using DOT.Core.Extensions;
+using DOT.Core.Isolation.System.Threading;
+using DOT.Core.Native;
+using Tick42;
+using Tick42.StartingContext;
+using Image = System.Drawing.Image;
+
+namespace io_connect_anything
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className,
+            string windowTitle);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, StringBuilder lParam);
+
+        // for clipboard to work, we have to be in STA thread
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Dispatcher.CurrentDispatcher.BeginInvoke((Action)MainThread);
+            Dispatcher.Run();
+        }
+
+        public static Image DrawText(string text, Font font, Color textColor, Color backColor)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            using (Bitmap dummyBitmap = new Bitmap(1, 1))
+            {
+                using (Graphics dummyGraphics = Graphics.FromImage(dummyBitmap))
+                {
+                    SizeF textSize = dummyGraphics.MeasureString(text, font);
+
+                    Bitmap bitmap = new Bitmap((int)textSize.Width, (int)textSize.Height);
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
+                    {
+                        graphics.Clear(backColor);
+
+                        using (Brush textBrush = new SolidBrush(textColor))
+                        {
+                            graphics.DrawString(text, font, textBrush, 0, 0);
+                        }
+                    }
+
+                    return bitmap;
+                }
+            }
+        }
+
+        public static void DrawTextAndPaste(string text, IntPtr intPtr)
+        {
+            var img = DrawText(text, new Font("Arial", 16), Color.Black, Color.White);
+            if (img == null)
+            {
+                return;
+            }
+
+            Clipboard.SetImage(img);
+            Win32.SetForegroundWindow(intPtr);
+            Thread.Sleep(100);
+            SendKeys.SendWait("^v");
+        }
+
+        private static async void MainThread()
+        {
+            var disp = Dispatcher.CurrentDispatcher;
+            var glue = await Glue42.InitializeGlue(new InitializeOptions
+            {
+                AppDefinition = new AppDefinition
+                {
+                    // we're just launcher for other apps, so we don't need to be saved and restored
+                    // otherwise we will be closed upon restoration
+                    // if this is desired - put this to false
+                    IgnoreFromLayouts = true
+                }
+            });
+            Console.WriteLine(":::glue ready");
+
+            await glue.AppManager.RegisterAppFactoryAsync<object, LambdaApp<string>, string, object>(app => app
+                    .WithFolder("Anything")
+                    .WithAllowMultiple(true)
+                    .WithName("Notepad")
+                    .WithChannelSupport(true)
+                    .WithDispatcher(disp),
+                async (context, builder, __) =>
+                {
+                    Console.WriteLine(":::launching notepad");
+                    Process notepad = Process.Start("notepad.exe");
+                    notepad.WaitForInputIdle();
+
+                    IntPtr notepadHandle = notepad.MainWindowHandle;
+                    IntPtr editHandle = FindWindowEx(notepadHandle, IntPtr.Zero, "Edit", null);
+
+                    return new LambdaApp<string>(notepadHandle)
+                    {
+                        Shutdown = () => notepad.Kill(),
+                        Init = state => Win32.SendMessage(editHandle, Win32.WM_SETTEXT, IntPtr.Zero, state),
+                        ChannelChanged = (channelContext, channel, arg3) =>
+                        {
+                            // or read from channelContext.GetValue<string>("partyPortfolio.ric");
+                            Win32.SendMessage(editHandle, Win32.WM_SETTEXT, IntPtr.Zero, channel.Name);
+                        },
+                        ChannelUpdate = (channelContext, channel, arg3) =>
+                        {
+                            var ric = channelContext.GetValue<string>("partyPortfolio.ric");
+                            Win32.SendMessage(editHandle, Win32.WM_SETTEXT, IntPtr.Zero, ric);
+                        },
+                        GetState = () =>
+                        {
+                            int length = (int)Win32.SendMessage(editHandle, Win32.WM_GETTEXTLENGTH, IntPtr.Zero,
+                                IntPtr.Zero);
+                            StringBuilder sb = new StringBuilder(length + 1);
+
+                            SendMessage(editHandle, Win32.WM_GETTEXT, (IntPtr)sb.Capacity, sb);
+                            return sb.ToString().AsCompletedTask();
+                        }
+                    };
+                });
+
+            await glue.AppManager.RegisterAppFactoryAsync<object, LambdaApp<string>, string, object>(app => app
+                    .WithFolder("Anything")
+                    .WithAllowMultiple(true)
+                    .WithName("Paint")
+                    .WithChannelSupport(true)
+                    .WithDispatcher(disp),
+                async (context, builder, __) =>
+                {
+                    Console.WriteLine(":::launching paint");
+                    Process paint = Process.Start("mspaint.exe");
+                    paint.WaitForInputIdle();
+
+                    // wait a bit for main window to be ready
+                    await Task.Delay(500);
+                    IntPtr paintHandle = paint.MainWindowHandle;
+
+                    return new LambdaApp<string>(paintHandle)
+                    {
+                        Shutdown = () => paint.Kill(),
+                        Init = state => DrawTextAndPaste("Loaded: " + state, paintHandle),
+                        ChannelChanged =
+                            (channelContext, channel, arg3) => DrawTextAndPaste(channel?.Name, paintHandle),
+                        ChannelUpdate = (channelContext, channel, arg3) =>
+                        {
+                            var ric = channelContext.GetValue<string>("partyPortfolio.ric");
+                            DrawTextAndPaste(ric, paintHandle);
+                        },
+                        GetState = () => Guid.NewGuid().ToString("N").AsCompletedTask()
+                    };
+                });
+            
+            Console.WriteLine(":::io-connect-anything ready");
+        }
+    }
+}
