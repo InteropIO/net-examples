40784cba959348d94112adba2d975301ed7857d3
Stanislav Peichev <stanislav.peichev@interop.io>
2024-10-14 18:13:04 +0300

    WebView2 POC

---
+++ WebView2/GlueWebView2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Tick42;
+using Tick42.Windows;
+
+namespace GlueWebView2
+{
+    [ComVisible(true)]
+    public class WebView2GlueContext
+    {
+        private readonly Glue42 glue_;
+        private readonly string windowId_;
+        private string callbackRegistration_;
+        private CoreWebView2 webView_;
+        private string token_;
+
+        public WebView2GlueContext(CoreWebView2 webView, Glue42 glue, string windowId)
+        {
+            webView_ = webView;
+            glue_ = glue;
+            windowId_ = windowId;
+
+            // we can create an auth token here and expose it instead of the username
+        }
+
+        public void RegisterCallback(string callbackId)
+        {
+            callbackRegistration_ = callbackId;
+        }
+
+        public string GetGwUri() => glue_.GatewayUri;
+
+        public string GetUsername() => glue_.Identity.UserName;
+
+        public string GetIdentity() => glue_.ProtocolSerializer.SerializeMessage(glue_.Identity);
+
+        public string GetWindowId() => windowId_;
+
+        public string CreateGwToken() => token_;
+
+        public void Notify(object data)
+        {
+            if (string.IsNullOrEmpty(callbackRegistration_))
+            {
+                return;
+            }
+
+            var message = glue_.ProtocolSerializer.SerializeMessage(new
+            {
+                @event = "callback",
+                callbackId = callbackRegistration_,
+                data
+            });
+            webView_.PostWebMessageAsJson(message);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private IGlueWindow window_;
+        private WebView2GlueContext wv2Context_;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitGlueAndWebView2().ContinueWith(_ => { });
+        }
+
+        private async Task InitGlueAndWebView2()
+        {
+            textBox1.Text = "http://localhost:8080/index.html";
+            textBox1.ReadOnly = true;
+
+            Text = "Registering Glue";
+            CancellationTokenSource ct = new CancellationTokenSource();
+            _ = Task.Run(async () =>
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    await Task.Delay(250, ct.Token);
+                    void Progress() => Text += ".";
+                    BeginInvoke((Action)Progress);
+                }
+            }, ct.Token);
+            var glue = await Glue42.InitializeGlue();
+            ct.Cancel();
+            Text = "Glue initialized";
+            window_ = await glue.GlueWindows.RegisterStartupWindowByHandle(Handle, "Glue WebView2",
+                o => o.WithChannelSupport(true));
+            webView21.CoreWebView2InitializationCompleted += (sender, args) =>
+            {
+                wv2Context_ = new WebView2GlueContext(webView21.CoreWebView2, glue, window_.Id);
+                webView21.CoreWebView2.AddHostObjectToScript("glue", wv2Context_);
+                textBox1.ReadOnly = false;
+            };
+            CoreWebView2Environment wv2Env = await CoreWebView2Environment.CreateAsync();
+            await webView21.EnsureCoreWebView2Async(wv2Env);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                webView21.CoreWebView2.Navigate(textBox1.Text);
+            }
+        }
+    }
+}
---
+++ WebView2/GlueWebView2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GlueWebView2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GlueWebView2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aaf4a58b-6902-479a-8ef5-8e83d7fb4df0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
