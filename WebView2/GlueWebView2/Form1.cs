bb3c4025c985abdc719686ada8c554b21821d6a8
Stanislav Peichev <stanislav.peichev@interop.io>
2024-10-15 15:15:33 +0300

    WebView2 POC - option to init or only discover glue

--- WebView2/GlueWebView2/Form1.cs
+++ WebView2/GlueWebView2/Form1.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DOT.AGM.GwTransport;
 using Microsoft.Web.WebView2.Core;
 using Tick42;
+using Tick42.StartingContext;
 using Tick42.Windows;
 
 namespace GlueWebView2
@@ -12,19 +15,34 @@ namespace GlueWebView2
     [ComVisible(true)]
     public class WebView2GlueContext
     {
-        private readonly Glue42 glue_;
         private readonly string windowId_;
         private string callbackRegistration_;
-        private CoreWebView2 webView_;
+        private string environment_;
+        private string gwUri_;
+        private string identityUserName_;
+        private string region_;
+        private IGwProtocolSerializer serializer_;
         private string token_;
+        private CoreWebView2 webView_;
 
-        public WebView2GlueContext(CoreWebView2 webView, Glue42 glue, string windowId)
+        public WebView2GlueContext(CoreWebView2 webView,
+            IGwProtocolSerializer serializer,
+            string windowId,
+            string environment,
+            string region,
+            string gwUri,
+            string username,
+            string token)
         {
             webView_ = webView;
-            glue_ = glue;
             windowId_ = windowId;
 
-            // we can create an auth token here and expose it instead of the username
+            serializer_ = serializer;
+            region_ = region;
+            environment_ = environment;
+            gwUri_ = gwUri;
+            identityUserName_ = username;
+            token_ = token;
         }
 
         public void RegisterCallback(string callbackId)
@@ -32,11 +50,13 @@ namespace GlueWebView2
             callbackRegistration_ = callbackId;
         }
 
-        public string GetGwUri() => glue_.GatewayUri;
+        public string GetGwUri() => gwUri_;
 
-        public string GetUsername() => glue_.Identity.UserName;
+        public string GetUsername() => identityUserName_;
 
-        public string GetIdentity() => glue_.ProtocolSerializer.SerializeMessage(glue_.Identity);
+        public string GetEnvironment() => environment_;
+
+        public string GetRegion() => region_;
 
         public string GetWindowId() => windowId_;
 
@@ -49,7 +69,7 @@ namespace GlueWebView2
                 return;
             }
 
-            var message = glue_.ProtocolSerializer.SerializeMessage(new
+            var message = serializer_.SerializeMessage(new
             {
                 @event = "callback",
                 callbackId = callbackRegistration_,
@@ -67,13 +87,16 @@ namespace GlueWebView2
         public Form1()
         {
             InitializeComponent();
-            InitGlueAndWebView2().ContinueWith(_ => { });
+            // true to initialize glue, false to use the discovery service
+            InitGlueAndWebView2(false).ContinueWith(_ => { });
         }
 
-        private async Task InitGlueAndWebView2()
+        private async Task InitGlueAndWebView2(bool initGlue)
         {
             textBox1.Text = "http://localhost:8080/index.html";
             textBox1.ReadOnly = true;
+
+            CreateHandle();
 
             Text = "Registering Glue";
             CancellationTokenSource ct = new CancellationTokenSource();
@@ -82,18 +105,54 @@ namespace GlueWebView2
                 while (!ct.IsCancellationRequested)
                 {
                     await Task.Delay(250, ct.Token);
-                    void Progress() => Text += ".";
+
+                    void Progress()
+                    {
+                        if (ct.IsCancellationRequested)
+                        {
+                            return;
+                        }
+
+                        Text += ".";
+                    }
+
                     BeginInvoke((Action)Progress);
                 }
             }, ct.Token);
-            var glue = await Glue42.InitializeGlue();
+
+            GDStartingContext gdsc;
+            IGwProtocolSerializer gwp = new GwProtocolSerializer(ValueGwConverter.Settings.None);
+            string gwToken = null;
+            if (!initGlue)
+            {
+                var ds = new FileGlueDesktopDiscoveryService();
+                GDStartingContext[] gdscs = await ds.Discover();
+                // choose some gdsc
+                gdsc = gdscs.FirstOrDefault();
+            }
+            else
+            {
+                var glue = await Glue42.InitializeGlue();
+                window_ = await glue.GlueWindows.RegisterStartupWindowByHandle(Handle, "Glue WebView2",
+                    o => o.WithChannelSupport(true));
+
+                // we can create an auth token here and expose it instead of the username
+                gdsc = glue.GDStartingContext;
+            }
+
             ct.Cancel();
-            Text = "Glue initialized";
-            window_ = await glue.GlueWindows.RegisterStartupWindowByHandle(Handle, "Glue WebView2",
-                o => o.WithChannelSupport(true));
+            Text = "Glue " + (gdsc != null ? "found: " + gdsc.GwURL : "not found");
+
+            if (gdsc == null)
+            {
+                // no reason to proceed
+                return;
+            }
+
             webView21.CoreWebView2InitializationCompleted += (sender, args) =>
             {
-                wv2Context_ = new WebView2GlueContext(webView21.CoreWebView2, glue, window_.Id);
+                wv2Context_ = new WebView2GlueContext(webView21.CoreWebView2, gwp, window_?.Id,
+                    gdsc.Env, gdsc.Region, gdsc.GwURL, gdsc.Username, gwToken);
                 webView21.CoreWebView2.AddHostObjectToScript("glue", wv2Context_);
                 textBox1.ReadOnly = false;
             };
@@ -103,6 +162,11 @@ namespace GlueWebView2
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            if (textBox1.ReadOnly)
+            {
+                return;
+            }
+
             if (e.KeyChar == (char)Keys.Enter)
             {
                 webView21.CoreWebView2.Navigate(textBox1.Text);
